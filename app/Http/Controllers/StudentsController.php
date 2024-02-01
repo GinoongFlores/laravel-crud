51144537451e185fe5a1b4fee43284f658d74bb1
ginoongflores <florespaul161@gmail.com>
2024-02-01 09:23:17 +0800

    feat(StudentsController): add as Controller

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Students;
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+    //
+    public function store(Request $request) {
+        Students::create([
+        'first_name'=>$request->first_name,
+        'last_name'=>$request->last_name,
+        'email'=>$request->email,
+        'course'=>$request->course
+        ]);
+        return response()->json('Successfully Created');
+    }
+
+    public function fetch() {
+        return response()->json(Students::latest()->get());
+    }
+
+    public function fetchStudent(string $id) {
+        return response()->json(Students::whereId($id)->first());
+    }
+
+    public function update(Request $request, string $id) {
+        $user = Students::whereId($id)->first();
+
+        $user->update([
+            'first_name'=>$request->first_name,
+            'last_name'=>$request->last_name,
+            'email'=>$request->email,
+            'course'=>$request->course,
+        ]);
+        return response()->json('Update Successfully');
+    }
+
+    public function delete($id) {
+        {
+            Students::whereId($id)->delete();
+            return response()->json(("Deleted Successfully"));
+        }
+    }
+
+}

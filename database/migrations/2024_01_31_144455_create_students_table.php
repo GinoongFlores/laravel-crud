ee7db663c2f3ccc7a0de4e7aeb62c97f6da7a54d
ginoongflores <florespaul161@gmail.com>
2024-02-01 09:25:20 +0800

    feat(create_students_table): create as table

--- database/migrations/2024_01_31_144455_create_students_table.php
+++ database/migrations/2024_01_31_144455_create_students_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string("first_name");
+            $table->string("last_name");
+            $table->string("email");
+            $table->string("course");
             $table->timestamps();
         });
     }

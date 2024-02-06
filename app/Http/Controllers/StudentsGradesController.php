6f99346cae946e677c173b5821c35df96f12f8a0
ginoongflores <florespaul161@gmail.com>
2024-02-06 10:48:25 +0800

    feat(StudentsGradesController): added

--- app/Http/Controllers/StudentsGradesController.php
+++ app/Http/Controllers/StudentsGradesController.php
@@ -9,13 +9,6 @@ class StudentsGradesController extends Controller
 {
     //
 
-    public function calculate_grades($grades) {
-        $total = array_sum($grades);
-        $average = $total / count($grades);
-
-        return ['total' => $total, 'average' => $average];
-    }
-
     public function store_grades(Request $request) {
         $validatedGrade = $request->validate([
             'subject_1' => 'required|numeric',
@@ -33,22 +26,50 @@ class StudentsGradesController extends Controller
             $validatedGrade['subject_5']
         ];
 
-        $calculatedGrades = $this->calculate_grades($grades);
+        $average = array_sum($grades) / count($grades);
+        $validatedGrade['average'] = $average;
 
-        $validatedGrade['total'] = $calculatedGrades['total'];
-        $validatedGrade['average'] = $calculatedGrades['average'];
+        Grades::create($validatedGrade);
 
-        $newGrade = Grades::create($validatedGrade);
         return redirect(route('student.grade'))->with('success', 'Grade created successfully');
     }
 
     public function create_grades() {
         $grades = Grades::latest()->get();
-        return view('students.grades', ['grades'=>$grades]);
+        return view('students.grades', ['grades' => $grades]);
     }
 
-    public function edit_grades(Grades $grades) {
-        return view('students.edit_grade', ['grades' => $grades]);
+    public function edit_grades(Grades $grade) {
+        return view('students.edit_grade', ['grade' => $grade]);
+    }
+
+    public function update_grades(Grades $grade, Request $request) {
+        $validatedGrade = $request->validate([
+            'subject_1' => 'required|numeric',
+            'subject_2' => 'required|numeric',
+            'subject_3' => 'required|numeric',
+            'subject_4' => 'required|numeric',
+            'subject_5' => 'required|numeric',
+        ]);
+
+        $gradesArray = [
+            $validatedGrade['subject_1'],
+            $validatedGrade['subject_2'],
+            $validatedGrade['subject_3'],
+            $validatedGrade['subject_4'],
+            $validatedGrade['subject_5']
+        ];
+
+        $average = array_sum($gradesArray) / count($gradesArray);
+        $validatedGrade['average'] = $average;
+
+        $grade->update($validatedGrade);
+        return redirect(route('student.grade'))->with('success', 'Grade updated successfully');
+    }
+
+    public function delete_grades(Grades $grade) {
+        $grade->delete();
+        return redirect('student.grade')->with('success', 'Grade deleted successfully');
     }
 
 
